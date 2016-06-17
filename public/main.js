8af68af7683384d7696c03f77a7c632cf601d7c0
RexCorpDev <rexcorpfx@gmail.com>
2016-06-17 00:25:52 -0700

    delete from FE works

--- public/main.js
+++ public/main.js
@@ -3,18 +3,24 @@
 $(() => {
   getPosts();
   $('.create-post').on('click', createPost);
+  $('.all-posts').on('click', '.delete-btn' ,deletePost);
 });
 
-let getPosts = () => {
-  $.get('/posts')
-  .done((data)=> {
-    let dbPosts = data;
-    console.log('dbData: ', dbPosts);
-    renderPosts(dbPosts);
+let deletePost = event => {
+  let $deleteId = event.toElement.parentElement.parentElement.parentElement.attributes.id.value;
+  console.log('$deleteId: ', $deleteId);
+  $.ajax({
+    url   : `/posts/${$deleteId}`,
+    type  : 'DELETE',
+
+  })
+  .done(()=> {
+    getPosts();
+  })
+  .fail(()=> {
+    alert('Failed To Delete');
   });
-}
-
-
+};
 let createPost = () => {
   event.preventDefault();
   let newAuthor = $('input.new-author').val();
@@ -27,13 +33,24 @@ let createPost = () => {
     getPosts();
   });
 };
-
+let getPosts = () => {
+  $.get('/posts')
+  .done((data)=> {
+    let dbPosts = data;
+    console.log('dbData: ', dbPosts);
+    renderPosts(dbPosts);
+  });
+}
 let renderPosts = dbPosts => {
+  $('.append-here').empty();
+  dbPosts.sort((a, b)=> {
+    return b.sortD - a.sortD;
+  });
   dbPosts.forEach(dbPost => {
     let $newPost = $('div.template').clone();
     $newPost.removeClass('template').addClass('new-post').attr('id', dbPost.id);
     $newPost.find('.template-author-name').removeClass('template-author-name').addClass('author-Name').text(dbPost.author);
-    let date = dbPost.date.slice(0, 24);
+    let date = dbPost.date.slice(0,24);
     $newPost.find('.template-time-stamp').removeClass('template-time-stamp').addClass('time-stamp').text(date);
     $newPost.find('.template-post-message').removeClass('template-time-stamp').addClass('post-message').text(dbPost.post);
     $('div.append-here').append($newPost);
